c52737b36a8c2f693c3f1f9182aca0c3b832c50e
matheus7git <matheus07gitteles@gmail.com>
2024-09-12 10:45:14 -0300

    projeto concluido

--- app.py
+++ app.py
@@ -26,6 +26,24 @@ def delete(id):
     requests.delete(f'{API_URL}/{id}')
     return redirect('/')
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        autor = request.form['autor']
+        requests.put(
+            f'{API_URL}/{id}',
+            json={'titulo':titulo, 'autor': autor}
+        )
+        return redirect('/')
+    
+    response = requests.get(f'{API_URL}/{id}')
+    if response.status_code == 200:
+        livro = response.json()
+    else:
+        livro = {}
+
+    return render_template('update.html', livro=livro)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
